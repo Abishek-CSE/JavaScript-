1b34c8224a7b3cf5d90dec583840c4bcc93a2cca
Abishek <abisheknavas500@gmail.com>
2025-10-14 19:10:21 +0530

    Add files via upload

---
+++ Conditional Statement/Else  if/else_if.js
@@ -0,0 +1,74 @@
+var age1 = 25;
+if (age1 <= 18) {
+    console.log("You can right");
+}
+else if (age1 == 25) {
+    console.log("Another Right");
+}
+else {
+    console.log("Wrong");
+}
+
+let a = 30;
+var b = 45;
+if (a == b) {
+    console.log("Equal");
+}
+else if (b > a) {
+    console.log("B is greater");
+}
+else {
+    console.log("A is greater");
+}
+let mobile = 12;
+let mobile_charger = 10;
+if (mobile_charger == mobile) {
+    console.log("Everyone can use");
+}
+else if (mobile <= mobile_charger) {
+    console.log("False")
+}
+else {
+    console.log("Some people cant use the charger properly");
+}
+if(mobile != mobile_charger){
+    console.log("Some Problem will happen");
+}
+else if(mobile <= mobile_charger){
+    console.log("Problem started")
+}
+else{
+    console.log("Problem Resolved");
+}
+
+let age =16;
+if(age <= 18){
+    console.log("Cant vote ");
+}
+else if(age >=45){
+    console.log("Anyone can vote")
+}
+else{
+    console.log("You can Vote");
+}
+
+if(age <=18){
+    console.log("You can Vote");
+}
+else if( age <=18 || age >=45){
+    console.log("You cant Vote");
+}
+else{
+    console.log("Anyone can vote")
+}
+const name = "Abishek";
+const name2 = "Abi"
+if(name ==name2){
+    Console.log("Matching");
+}
+else if(name != name2){
+    console.log("Something Not Matched");
+}
+else{
+    console.log("Not matching");
+}
---
+++ Conditional Statement/If-else/if.js
@@ -0,0 +1,69 @@
+let first_number = 10;
+if(first_number == 10){
+    console.log(first_number);
+}
+
+let second_number = 20;
+if(second_number <=10){
+    console.log(second_number);
+}
+console.log("False Statement");
+
+let third_number =30;
+if(third_number >=40){
+    console.log(third_number);
+}
+console.log("False Statement");
+
+let fourth_number =40;
+fourth_number =45;
+if(fourth_number === 45){
+    console.log(fourth_number);
+}
+
+var fifth_number =50;
+var fifth_number =55;
+if(fifth_number > 50){
+    console.log(fifth_number);
+}
+
+if(fifth_number < 50){
+    console.log(fifth_number);
+}
+
+let result = first_number+=9;
+if(first_number == 19){
+    console.log(first_number);
+}
+
+if(first_number <= second_number){
+    console.log("True Statement")
+}
+
+let first_number1 = 10;
+if(first_number = first_number1){
+    console.log("Correct Statement");
+}
+
+
+let a =24;
+let b=26;
+let c= a+b;
+if(c = a+b){
+    console.log("Perfect Statement");
+}
+
+let note =23;
+if(note != 25){
+    console.log("Correct Statement");
+}
+
+if(note =23 || note >25){
+    console.log("Good");
+}
+if(note = 23 && note <25){
+    console.log(note);
+}
+if(note =23 && note > 25){
+    console.log(note);
+}
