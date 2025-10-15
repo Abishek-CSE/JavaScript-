1a4e4338290cdbf54a88070ff1262ff2f92e760d
Abishek <abisheknavas500@gmail.com>
2025-10-15 17:52:47 +0530

    Add files via upload

---
+++ Syntax.js
@@ -0,0 +1,23 @@
+//Looping Statement
+
+
+//For Loop Syntax:
+//for(initialization;condtion;increment/decrement){
+    //printing statement
+//}
+
+
+// While Loop Syntax
+//while(condition){
+    //printing Statement
+    //increment/decrement
+//}
+
+
+// Do-While Loop Syntax:
+//do{
+    //printing  statement
+    //increment/decrement
+//} while(condition){
+    //printing Statement
+//}
