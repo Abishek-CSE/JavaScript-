6926c7538142d67dd531029e431554a109b55ab0
Abishek <abisheknavas500@gmail.com>
2025-10-21 12:28:22 +0530

    Add files via upload

---
+++ Array/Array.js
@@ -0,0 +1,65 @@
+//Array => Collection of Data Types
+// Types of Array
+//1. One Dimensional Array
+//2. Two Dimensional Array
+//3. Multi Dimensional Array
+
+let movie=["Leo",96,"Dragon","Bison","I"]
+console.log(movie[0]);
+console.log(movie[1]);
+console.log(movie[2]);
+console.log(movie[3]);
+console.log(movie[4]);
+let number=[1,2,3,4,5]
+console.log(number[0]);
+console.log(number[1]);
+console.log(number[2]);
+console.log(number[3]);
+console.log(number[4]);
+let director=["Vetrimaaaran","Dhanush","MariSelvaraj","Selvaraghavan","Ashwath Marimuthu"]
+console.log(director[0]);
+console.log(director[1]);
+console.log(director[2]);
+console.log(director[3]);
+console.log(director[4]);
+let actor=["Vijay","STR","AK","Suriya","SK","Kamal","Rajini"]
+console.log(actor[0]);
+console.log(actor[1]);
+console.log(actor[2]);
+console.log(actor[3]);
+console.log(actor[4]);
+console.log(actor[5]);
+console.log(actor[6]);
+let Youtuber=["Vj Siddhu","Gosu Vlogs","Madan Gowri","Expert FX"]
+console.log(Youtuber[0]);
+console.log(Youtuber[1]);
+console.log(Youtuber[2]);
+console.log(Youtuber[3]);
+let sports=["Football","Cricket",["Bat","Ball","LegPad","Gloves","Helmet"],"Boxing","Dead Lifting"]
+console.log(sports[0]);
+console.log(sports[1]);
+console.log(sports[2]);
+console.log(sports[2][0]);
+console.log(sports[2][1]);
+console.log(sports[2][2]);
+console.log(sports[2][3]);
+console.log(sports[2][4]);
+console.log(sports[3]);
+console.log(sports[4]);
+let classroom=[1,2,[3,4,5,6]]
+console.log(classroom[0]);
+console.log(classroom[1]);
+console.log(classroom[2][0]);
+console.log(classroom[2][1]);
+console.log(classroom[2][2]);
+console.log(classroom[2][3]);
+let mixed=[1,[true,false],["AK","STR","SK"],"Boxing"]
+console.log(mixed[0]);
+console.log(mixed[2][0]);
+console.log(mixed[2][1]);
+console.log(mixed[3][0]);
+console.log(mixed[3][1]);
+console.log(mixed[3][2]);
+console.log(mixed[4]);
+
+
