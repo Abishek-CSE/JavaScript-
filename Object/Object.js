5e246ea42f4a46805e30d4f77b61bfb6f5859de5
Abishek <abisheknavas500@gmail.com>
2025-10-21 12:44:04 +0530

    Add files via upload

---
+++ Object/Object.js
@@ -0,0 +1,43 @@
+let vertimaaran={Name:"Vertrimaaran",Upcoming:"Arasan",Actor:"STR",Music:"Ani"}
+console.log(vertimaaran.Name);
+console.log(vertimaaran.Upcoming);
+console.log(vertimaaran.Actor);
+console.log(vertimaaran.Music);
+let Member={Name:"Abi",Age:22,Food:"Briyani"}
+console.log(Member.Name);
+console.log(Member.Age);
+console.log(Member.Food);
+let Dude={Name:"Agan",Story:"Love",Music:"Sai"}
+console.log(Dude.Name);
+console.log(Dude.Story);
+console.log(Dude.Music);
+const car = {type:"Fiat", model:"500", color:"white"};
+console.log(car.type);
+console.log(car.model);
+console.log(car.color);
+const person = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"};
+console.log(person.firstName);
+console.log(person.lastName);
+console.log(person.age);
+console.log(person.eyeColor)
+var cinema ={Director:"VetriMaaran",Music:{Rockstar:"Ani",Upcoming:"Sai",Liitle_Mastro:"U1"}}
+console.log(cinema.Director);
+console.log(cinema.Music.Rockstar);
+console.log(cinema.Music.Liitle_Mastro);
+console.log(cinema.Music.Upcoming);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
