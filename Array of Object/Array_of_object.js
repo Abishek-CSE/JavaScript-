a5041fff224a2d16bcbc8489f382bbe165ed608c
Abishek <abisheknavas500@gmail.com>
2025-10-21 13:40:52 +0530

    Add files via upload

---
+++ Array of Object/Array_of_object.js
@@ -0,0 +1,18 @@
+let cinema=[{MovieName:"Dude",Actor:"PR",Music:"Sai",Director:"Keerthi"},{MovieName:"Bison",Actor:"Dhruv Vikram",Music:"Nivas",Director:"MariSelvaraj"}]
+console.log(cinema[0].MovieName)
+console.log(cinema[0].Actor)
+console.log(cinema[0].Music)
+console.log(cinema[0].Director)
+console.log(cinema[1].MovieName)
+console.log(cinema[1].Actor)
+console.log(cinema[1].Music)
+console.log(cinema[1].Director)
+let cars = [{color: "purple",type: "minivan",registration:'2017-01-03',capacity: 7},{color: "red",type: "station wagon",registration:'2018-03-03',capacity: 5}]
+console.log(cars[0].color);
+console.log(cars[0].type);
+console.log(cars[0].registration);
+console.log(cars[0].capacity);
+console.log(cars[1].color);
+console.log(cars[1].registration);
+console.log(cars[1].capacity);
+console.log(cars[1].type);
